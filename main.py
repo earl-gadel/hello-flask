55b2a0bd479715ca4862d4b1eff98a2cdba9ebc9
Earl <earljg@gmail.com>
2018-03-25 16:58:04 -0500

    Class 2.5 Prep work and ignored two files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-import vigenere
+from flask import Flask, request, redirect
+#import vigenere
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -17,14 +17,78 @@ form = """
 </html>
 """
 
-@app.route("/")
-def index():
-    return form
+time_form = """
+    <style>
+        .error {{ color: red;}}
+    </style>
+    <h1>Validate Time</h1>
+    <form method='POST'>
+        <label>Hours (24-hour format)
+            <input name="hours" type="text" value='{hours}' />
+        </label>
+        <p class="error">{hours_error}</p>
+        <label>Minutes
+            <input name="minutes" type="text" value='{minutes}' />
+        </label>
+        <p class="error">{minutes_error}</p>
+        <input type="submit" value="Convert" />
+    </form>
+"""
+@app.route('/validate-time')
+def display_time_form():
+    return time_form.format(hours='', hours_error='', minutes='', minutes_error='')
 
-@app.route("/hello", methods=["post"])
-def hello():
-    first_name = request.form["first_name"]
-    return vigenere("first_name")
+def is_integer(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+@app.route('/validate-time', methods=['POST'])
+def validate_time():
+
+    hours = request.form['hours']
+    minutes = request.form['minutes']
+
+    hours_error = ''
+    minutes_error = ''
+
+    if not is_integer(hours):
+        hours_error = 'Not a valid integer'
+        hours = ''
+    else:
+        hours = int(hours)
+        if hours > 23 or hours < 0:
+            hours_error = 'Hour value out of range (0-23)'
+            hours = ''
+
+    if not is_integer(minutes):
+        minutes_error = 'Not a valid integer'
+        minutes = ''
+    else:
+        minutes = int(minutes)
+        if minutes > 59 or minutes < 0:
+            minutes_error = 'Minutes value out of range (0-59)'
+            minutes = ''
+
+    if not minutes_error and not hours_error:
+        return redirect('/valid-time')
+    else:
+        time_form.format(hours_error=hours_error, minutes_error=minutes_error, hours=hours, minutes=minutes)
+
+@app.route('/valid-time')
+def valid_time():
+    return '<h1>Thanks for submitting a valid time!</h1>'
+
+#@app.route("/")
+#def index():
+#    return form
+
+#@app.route("/hello", methods=["post"])
+#def hello():
+    #first_name = request.form["first_name"]
+    #return vigenere("first_name")
 
 
 app.run()
