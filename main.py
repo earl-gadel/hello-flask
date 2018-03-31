f6f94f3b0bdceb7dded9e0f37624a219a1ebf7c2
Earl <earljg@gmail.com>
2018-03-31 11:29:35 -0500

    created base.html for template extensions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -79,5 +79,17 @@ def valid_time():
     time = request.args.get('time')
     return '<h1>You submitted {0}. Thanks for submitting a valid time!</h1>'.format(time)
 
+tasks = []
+
+@app.route('/todos', methods=['POST', 'GET'])
+def todos():
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+
+
+    template = jinja_env.get_template('todos.html')
+    return template.render(title="TODOs", tasks=tasks)
 
 app.run()
